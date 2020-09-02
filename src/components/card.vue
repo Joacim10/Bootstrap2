aed98842a5a52ed39be2609b59dd78cf765a1caa
Joacim Hammarström <joacimhammarstrom@gmail.com>
2020-09-02 14:37:37 +0200

    7 commit

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="card-main">
-        <div class="card-original">
             <img src="https://placehold.it/200x200"/>
-            <div class="card-carousel--card--footer card-body">
+        <div class="card-original card-body card-carousel--card--footer">
                 <p>{{ item.name }}</p>
                 <p class="tag" v-for="(tag,index) in item.tag" :key="tag" :class="index &gt; 0 ? 'secondary' : ''">{{ item.tag[index] }}</p>
-            </div>
         </div>
-        <div class="card-hover d-none">
-            <img class="img" src="https://placehold.it/200x200"/>
-            <div class="card-carousel--card--footer card-body">
-                <p>{{ item.name }}</p>
-                <p class="tag" v-for="(tag,index) in item.tag" :key="tag" :class="index &gt; 0 ? 'secondary' : ''">{{ item.tag[index] }}</p>
-                <p>150$</p>
+        <div class="card-hover d-none card-body">
+            <div class="top-right">
+                <div>hej</div>
+                <div>på</div>
+                <div>dig</div>
+            </div>
+            <div class="bottom-left">
+                på dig
             </div>
         </div>
     </div>
@@ -26,9 +26,48 @@ export default {
 </script>
 
 <style scoped>
+
+    .top-right {
+        position:absolute;
+        right: 0px;
+        top: 0px;
+        display: flex;
+        flex-direction: column;
+        justify-content: between;
+        background-color: white;
+        border-radius: 20px;
+        padding: 5px;
+        margin: 5px;
+    }
+
+    .bottom-left {
+        position:absolute;
+        bottom: 0px;
+        left: 0px;
+    }
+
+    .card-main {
+        position: relative;
+    }
+
     .card-body{
         background-color: #0E153D;
+        position: absolute;
+        z-index: +1;
     }
+
+    .card-original {
+        bottom: 0px;
+        width: 100%;
+    }
+
+    .card-hover {
+        opacity: 0.9;
+        bottom: 0px;
+        height: 100%;
+        width: 100%;
+    }
+
     .card-main:hover > .card-original {
         display: none;
     }
@@ -36,8 +75,6 @@ export default {
         display: block !important;
     }
 
-    .card-hover {
-        background-color: #0E153D;
-    }
+
 
 </style>
