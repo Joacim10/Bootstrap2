241c30abd358d6db8108d0b42d7528fbf4ff3f7c
Joacim Hammarström <joacimhammarstrom@gmail.com>
2020-09-11 11:44:19 +0200

    41 commit

--- src/components/elements/subscribe.vue
+++ src/components/elements/subscribe.vue
@@ -4,9 +4,9 @@
             <div class="my-5">
                 <h2 class="d-inline-block">SUBSCRIBE <h2 class="text-info d-inline-block"> NOW </h2></h2>
                 <p class="w-md-75">Signup for awesome news about our new products and know our special discounts on products.</p>
-                <form action="">
-                    <input class="" type="text">
-                    <button class="btn rounded btn-info">SUBSCRIBE</button>
+                <form id="form" action="">
+                    <input class="rounded" type="text" placeholder="Enter email address">
+                    <button class="btn rounded btn-info">Subscribe</button>
                 </form>
             </div>
             <div class="d-none d-md-block bg-grey w-100">
@@ -21,6 +21,31 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+form {
+    width: 100%;
+    position: relative;
+    padding-right: 10px;
+}
+
+input {
+    padding: 10px;
+    margin: 0;
+    width: 100%;
+    border: 1px solid #c7c7c7;
+}
+
+input::placeholder {
+    color: #c7c7c7;
+}
+
+.btn {
+    position: absolute;
+    right: 15px;
+    margin: 0;
+    top: 5px;
+    border: none;
+}
 
 </style>
